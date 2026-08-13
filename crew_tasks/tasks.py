1f5b7e96a7746a2a83a9ee7204fcd797b8a4bd56
VishSeran <vishwa.sovis@gmail.com>
2026-08-13 19:43:23 +0530

    agents-crew task updated

---
+++ crew_tasks/tasks.py
@@ -0,0 +1,23 @@
+
+
+from configs.logger import get_logger
+
+
+logger = get_logger("tasks")
+
+class CrewTasks:
+    
+    def __init__(self):
+        
+        
+        try:
+            
+            self.research_task = 
+            
+        except ValueError:
+            logger.exception("Value error in crew tasks init")
+            raise
+        
+        except Exception:
+            logger.exception("Error in crew tasks init")
+            raise
