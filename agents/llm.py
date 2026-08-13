3e1fe69030262bc59ca4ea0fa531463b65444ddb
VishSeran <vishwa.sovis@gmail.com>
2026-08-13 19:04:39 +0530

    agents-llm handler updated

--- agents/llm.py
+++ agents/llm.py
@@ -1,3 +1,5 @@
+import os
+import dotenv
 
 from crewai import LLM
 
@@ -6,17 +8,27 @@ from configs.config import GROQ_MODEL
 
 
 logger = get_logger("llm")
-
+dotenv.load_dotenv()
 class LLMHandler:
     
-    def __init__(self):
+    def __init__(self, model_name=GROQ_MODEL):
         
         try:
+            if not model_name:
+                raise ValueError("Model name is missing")
             
+            groq_api = os.getenv("groq_api")
+            
+            if not groq_api:
+                raise ValueError("Groq api is missing")
         
             self.llm = LLM(
-                model=GROQ_MODEL
+                model=GROQ_MODEL,
+                api_key=groq_api,
+                max_tokens=4000
             )
+            
+            logger.info("llm is initialized")
             
         except ValueError as e:
             logger.exception("Value error in llm handler")
