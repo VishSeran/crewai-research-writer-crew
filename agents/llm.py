c01efbcd807a5e37ed2bb9eb8d8bf6e487826ddf
VishSeran <vishwa.sovis@gmail.com>
2026-08-13 19:01:39 +0530

    agents-llm handler updated

--- agents/llm.py
+++ agents/llm.py
@@ -1,2 +1,27 @@
 
+from crewai import LLM
 
+from configs.logger import get_logger
+from configs.config import GROQ_MODEL
+
+
+logger = get_logger("llm")
+
+class LLMHandler:
+    
+    def __init__(self):
+        
+        try:
+            
+        
+            self.llm = LLM(
+                model=GROQ_MODEL
+            )
+            
+        except ValueError as e:
+            logger.exception("Value error in llm handler")
+            raise
+        
+        except Exception as e:
+            logger.exception("Error in llm handler")
+            raise
