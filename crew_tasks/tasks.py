a7193777596e53c098ca32df11a0cbc29cc8a77f
VishSeran <vishwa.sovis@gmail.com>
2026-08-13 19:48:15 +0530

    agents-crew task updated

--- crew_tasks/tasks.py
+++ crew_tasks/tasks.py
@@ -1,6 +1,9 @@
 
+from crewai import Task
 
 from configs.logger import get_logger
+from agents.research_agent import ResearchAgent
+from agents.writer_agent import WriterAgent
 
 
 logger = get_logger("tasks")
@@ -12,7 +15,13 @@ class CrewTasks:
         
         try:
             
-            self.research_task = 
+            self.research_agent = ResearchAgent().research_agent
+            self.writer_agent =  WriterAgent().writer_agent
+            
+            self.research_task = Task(
+                description="analyze the major {topic}, identifying key trends and technologies. Provide a detailed report on their potential impact.",
+                agent=ResearchAgent
+            )
             
         except ValueError:
             logger.exception("Value error in crew tasks init")
