9927fc934680a38e5e9fd56c36a62fd6c58099a6
VishSeran <vishwa.sovis@gmail.com>
2026-08-14 13:44:46 +0530

    main updated

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 
+from app import gradio_interface
 from configs.logger import get_logger
 
 
 logger = get_logger("main")
+
+
+if __name__ == "__main__":
+    
+    print("Application is starting....")
+    gradio_interface()
 
 
 
